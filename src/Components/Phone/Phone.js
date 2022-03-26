1280a7f037571de655fd5f7b3ea54eff0c201390
kmyeasin <kmyeasin29>
2022-03-26 13:52:30 +0600

    Event Handler

--- src/Components/Phone/Phone.js
+++ src/Components/Phone/Phone.js
@@ -1,15 +1,23 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
+
 import './Phone.css'
 
 const Phone = (props) => {
+    const { handleAddToCart } = props;
     const { name, img, price } = props.phone
     return (
-        <div>
-            <div className="product">
-                <img src={img} alt="" />
+        <div className="product">
+            <img src={img} alt="" />
+            <div>
+                <h2>{name}</h2>
+                <h5>Price: ${price}</h5>
             </div>
-            <h2>{name}</h2>
-            <h5>Price: ${price}</h5>
+            <button onClick={() => handleAddToCart(props.phone)} className='cart-button'>
+                <p className='btn-txt'>Add To Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
+            </button>
         </div>
     );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -3,28 +3,36 @@ import Phone from '../Phone/Phone';
 import './Shop.css'
 const Shop = () => {
     const [phones, setPhones] = useState([]);
+    const [cart, setCart] = useState([])
     useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setPhones(data))
     }, [])
+    const handleAddToCart = (phone) => {
+        // console.log(phone);
+        const newCart = [...cart, phone]
+        setCart(newCart)
+    }
     return (
 
         <div>
             <h1>NOKIA PHONE COLLECTION</h1>
 
-            <div className='shop'>
+            <div className="shop">
                 <div className="products-container">
                     {
                         phones.map(phone => <Phone
                             key={phone.id}
                             phone={phone}
+                            handleAddToCart={handleAddToCart}
                         ></Phone>)
                     }
                 </div>
-            </div>
-            <div className="cart-container">
-                <h3>Order Summery</h3>
+                <div className="cart-container">
+                    <h3>Order Summery</h3>
+                    <p>Add Items: {cart.length}</p>
+                </div>
             </div>
         </div>
     );
